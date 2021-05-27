14404b058b4664e3d42f9e500627da5f747ef2f7
Paahn <paahndev@gmail.com>
2021-05-27 11:36:49 -0700

    using our state function to set the state from the data returning from the API

--- src/App.js
+++ src/App.js
@@ -4,40 +4,16 @@ import Pokemon from './components/Pokemon';
 
 
 const App = () => {
-  const [pokemon, setPokemon] = useState([
-    {
-        "name": "bulbasaur",
-        "url": "https://pokeapi.co/api/v2/pokemon/1/"
-    },
-    {
-        "name": "ivysaur",
-        "url": "https://pokeapi.co/api/v2/pokemon/2/"
-    },
-    {
-        "name": "venusaur",
-        "url": "https://pokeapi.co/api/v2/pokemon/3/"
-    },
-    {
-        "name": "charmander",
-        "url": "https://pokeapi.co/api/v2/pokemon/4/"
-    },
-    {
-        "name": "charmeleon",
-        "url": "https://pokeapi.co/api/v2/pokemon/5/"
-    },
-    {
-        "name": "charizard",
-        "url": "https://pokeapi.co/api/v2/pokemon/6/"
-    }
-  ]);
+  const [pokemon, setPokemon] = useState({});
 
   const getPokemon = async () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+    const url = "https://pokeapi.co/api/v2/pokemon/kabutops";
 
     await fetch(url)
     .then ((response) => response.json())
     .then ((data) => {
       console.log(data);
+      setPokemon(data);
     })
   };
 
@@ -51,7 +27,7 @@ const App = () => {
         width: 50%;
         text-align: center;
     `}>
-      <h1>Pokedexter</h1>
+      <h1>Pokedex</h1>
       <Pokemon pokemon={pokemon} />
     </div>
   );
