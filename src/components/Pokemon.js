1a201c5b05e446e59184047c129ed93dbc92235b
Paahn <paahndev@gmail.com>
2021-05-27 15:08:53 -0700

    implemented short circuiting to correctly handle loading the pokemon card after the Ajax call returns

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,13 +2,7 @@ import React from 'react';
 import { css } from '@emotion/css';
 
 const Pokemon = ({ pokemon }) => {
-  if(pokemon.sprites === undefined){
-    return(
-      <div>
-          Loading...
-      </div>
-    );
-}
+  
     return (
         <div className={css`
             margin: auto;
@@ -19,22 +13,27 @@ const Pokemon = ({ pokemon }) => {
         `}
         >
             <div>
-              <img src={pokemon.sprites.front_default} alt='default-sprite'></img>
-              <p>name: {pokemon.name}</p>
-              <p>height: {pokemon.height}</p>
-              <p>weight: {pokemon.weight}</p>
-              <p>base xp: {pokemon.base_experience}</p>
-              {pokemon.abilities.map((ab) =>
-              <p key={ab.ability.name}>Ability: {ab.ability.name}</p>
-              )}
-              {pokemon.types.map((type) => 
-              <p key={type.type.name}>Type: {type.type.name}</p>
-              )}
-              <div>
-                {pokemon.stats.map((stat) =>
-                <p key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</p>
-                )}
+              {
+               pokemon.sprites && 
+               <div> 
+                  <img src={pokemon.sprites.front_default} alt='default-sprite'></img>
+                  <p>name: {pokemon.name}</p>
+                  <p>height: {pokemon.height}</p>
+                  <p>weight: {pokemon.weight}</p>
+                  <p>base xp: {pokemon.base_experience}</p>
+                  {pokemon.abilities.map((ab) =>
+                  <p key={ab.ability.name}>Ability: {ab.ability.name}</p>
+                  )}
+                  {pokemon.types.map((type) => 
+                  <p key={type.type.name}>Type: {type.type.name}</p>
+                  )}
+                  <div>
+                    {pokemon.stats.map((stat) =>
+                    <p key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</p>
+                    )}
+                  </div>
               </div>
+              }
             </div>
         </div>
     )
