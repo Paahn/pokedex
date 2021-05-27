f7c66b10250b27f40cd6d5bcea2a3747ccf0ddcd
Paahn <paahndev@gmail.com>
2021-05-26 19:16:56 -0700

    hardcoded some data for our pokemon state
    and displaying it on the screen temporarily

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [pokemon, setPokemon] = useState([
+    {
+        "name": "bulbasaur",
+        "url": "https://pokeapi.co/api/v2/pokemon/1/"
+    },
+    {
+        "name": "ivysaur",
+        "url": "https://pokeapi.co/api/v2/pokemon/2/"
+    },
+    {
+        "name": "venusaur",
+        "url": "https://pokeapi.co/api/v2/pokemon/3/"
+    },
+    {
+        "name": "charmander",
+        "url": "https://pokeapi.co/api/v2/pokemon/4/"
+    },
+    {
+        "name": "charmeleon",
+        "url": "https://pokeapi.co/api/v2/pokemon/5/"
+    },
+    {
+        "name": "charizard",
+        "url": "https://pokeapi.co/api/v2/pokemon/6/"
+    }
+  ]);
   return (
     <div className="App">
       <h1>Pokedexter</h1>
+      {pokemon.map(pkm => 
+        <div>
+          {pkm.name}
+        </div>
+      )}
     </div>
   );
 }
